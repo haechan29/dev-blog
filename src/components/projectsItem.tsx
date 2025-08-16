93035a63fc11e94997575ef7b6fc61d4ae9823cc
haechan29 <haechan.im@gmail.com>
2025-08-16 19:50:48 +0900

    apply scale changing anim to project item

--- src/components/projectsItem.tsx
+++ src/components/projectsItem.tsx
@@ -5,6 +5,7 @@ import { ProjectDto } from '@/features/project/dto/projectsDto';
 import { toDomain } from '@/features/project/mapper/projectMapper';
 import { ProjectItemProps } from '@/features/project/ui/projectItemProps';
 import clsx from 'clsx';
+import { AnimatePresence, motion } from 'framer-motion';
 import { ArrowRight } from 'lucide-react';
 import { useEffect, useMemo, useState } from 'react';
 
@@ -46,7 +47,7 @@ export default function ProjectsItem() {
   }, []);
 
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col mb-20'>
       <div className='flex mb-10'>
         {PLATFORM_FILTERS.map(((platform, i) => {
           return (
@@ -70,37 +71,44 @@ export default function ProjectsItem() {
       </div>
 
       <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-        {projectItemProps !== null && projectItemProps.map(project => {
-          return (
-            <button
-              key={project.title}
-              className='relative aspect-square min-w-[200px] bg-gray-50 overflow-hidden group'
-            >
-              <div className='absolute inset-0 p-4'>
-                <img
-                  src={project.thumbnail}
-                  alt={`${project.title} project thumbnail`}
-                  className='w-full h-full object-contain object-center'
-                />
-              </div>
-              <div className='absolute inset-0 pointer-events-none flex flex-col items-start
-                bg-black/60 text-white p-4 group-hover:opacity-100 opacity-0 transition-opacity duration-300 ease-in-out
-              '>
-                <div className='text-lg font-bold'>{project.title}</div>
-                <div className='flex text-sm'>
-                  <div className='mr-1'>{project.platform}</div>
-                  <div>{`(${project.tools.join(', ')})`}</div>
+        <AnimatePresence mode='popLayout' initial={false}>
+          {projectItemProps !== null && projectItemProps.map(project => {
+            return (
+              <motion.button
+                key={project.title}
+                layout
+                initial={{ opacity: 0, scale: 0.3 }}
+                animate={{ opacity: 1, scale: 1.0 }}
+                exit={{ opacity: 0, scale: 0.3 }}
+                transition={{ duration: 0.3, ease: 'easeInOut' }}
+                className='relative aspect-square min-w-[200px] bg-gray-50 overflow-hidden group'
+              >
+                <div className='absolute inset-0 p-4'>
+                  <img
+                    src={project.thumbnail}
+                    alt={`${project.title} project thumbnail`}
+                    className='w-full h-full object-contain object-center'
+                  />
                 </div>
-                <div className='flex flex-1 w-full justify-end items-end'>
-                  <div className='flex items-center'>
-                    <div className='text-sm mr-1'>자세히 보기</div>
-                    <ArrowRight className='w-4 h-4' />
+                <div className='absolute inset-0 pointer-events-none flex flex-col items-start
+                  bg-black/60 text-white p-4 group-hover:opacity-100 opacity-0 transition-opacity duration-300 ease-in-out
+                '>
+                  <div className='text-lg font-bold'>{project.title}</div>
+                  <div className='flex text-sm'>
+                    <div className='mr-1'>{project.platform}</div>
+                    <div>{`(${project.tools.join(', ')})`}</div>
+                  </div>
+                  <div className='flex flex-1 w-full justify-end items-end'>
+                    <div className='flex items-center'>
+                      <div className='text-sm mr-1'>자세히 보기</div>
+                      <ArrowRight className='w-4 h-4' />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </button>
-          );
-        })}
+              </motion.button>
+            );
+          })}
+        </AnimatePresence>
       </div>
     </div>
   );
