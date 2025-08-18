12b47eed20d434c66266af08ca2caeb3ac629cc9
haechan29 <haechan.im@gmail.com>
2025-08-18 13:53:08 +0900

    change layout UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 import CapabilityItem from '@/components/capabilityItem';
-import IntroductionIem from '@/components/introductionItem';
+import IntroductionItem from '@/components/introductionItem';
 import ProjectsItem from '@/components/projectsItem';
 
 export default function HomePage() {
   return (
     <div className='min-h-screen bg-white'>
       <header className='sticky top-0 z-40 border-b-gray-200 border-b bg-white/80 backdrop-blur'>
-        <div className='mx-auto max-w-4xl px-4 py-3 flex gap-20 font-semibold'>
+        <div className='mx-auto max-w-6xl px-4 py-3 flex gap-20 font-semibold'>
           <div>홈</div>
           <div>포트폴리오</div>
           <div>블로그</div>
         </div>
       </header>
 
-      <div className='mx-auto max-w-4xl px-4 py-20 flex flex-col'>
-        <IntroductionIem />
+      <div className='mx-auto max-w-6xl px-4 py-20 flex flex-col'>
+        <IntroductionItem />
         <CapabilityItem />
         <ProjectsItem />
       </div>
--- src/components/introductionItem.tsx
+++ src/components/introductionItem.tsx
@@ -1,38 +1,55 @@
 import Image from 'next/image';
 
-export default function IntroductionIem() {
+type IntroductionItemProps = {
+  period: string;
+  description: string;
+}
+
+const introductionItemProps: IntroductionItemProps[] = [
+  {
+    period: '2023.04 - 2023.12', 
+    description: '소프트웨어 마에스트로'
+  },
+  {
+    period: '2024.06 - 2025.07', 
+    description: '캐시닥 안드로이드 개발자'
+  },
+  {
+    period: '현재', 
+    description: '프론트엔드 개발자로서 성장 중'
+  }
+]      
+
+export default function IntroductionItem() {
   return (
-    <div className='flex mb-30'>
-      <Image 
-        src='/images/programmer-looking.png'
-        alt='picture of me'
-        width={160}
-        height={160}
-        className='mr-20'/>
-      <div className='flex flex-1 flex-col justify-center'>
-        <div className='text-4xl ml-2 mb-4'>임해찬</div>
-        <div className='flex w-full relative mb-2'>
-          <div className='absolute w-full h-0.5 top-1/2 -translate-y-1/2 bg-gray-200 rounded-full'/>
-          <div className='flex w-full justify-between ml-4 z-10'>
-            <div className='w-3 h-3 rounded-full border-gray-300 border-2 bg-white'></div>
-            <div className='w-3 h-3 rounded-full border-gray-300 border-2 bg-white'></div>
-            <div className='w-3 h-3 rounded-full border-gray-300 border-2 bg-white'></div>
-            <div/>
-          </div>
+    <div className='flex flex-col mb-60'>
+      <div className='flex justify-center items-center mb-20'>
+        <Image 
+          src='/images/programmer-looking.png'
+          alt='picture of me'
+          width={160}
+          height={160}
+          className='mr-20'/>
+        <div className='flex items-end'>
+          <div className='text-7xl mr-6'>임해찬</div>
+          <div className='text-5xl text-gray-500 font-light'>Im Haechan</div>
         </div>
-        <div className='flex ml-4'>
-          <div className='flex flex-1 flex-col -ml-1 pr-2'>
-            <div className='text-sm text-gray-500'>2023.04 - 2023.12</div>
-            <div className='font-semibold'>소프트웨어 마에스트로</div>
-          </div>
-          <div className='flex flex-1 flex-col -ml-1 pr-2'>
-            <div className='text-sm text-gray-500'>2024.06 - 2025.07</div>
-            <div className='font-semibold'>캐시닥 안드로이드 개발자</div>
-          </div>
-          <div className='flex flex-1 flex-col -ml-1 pr-2'>
-            <div className='text-sm text-gray-500'>현재</div>
-            <div className='font-semibold'>프론트엔드 개발자로서 성장 중</div>
-          </div>
+      </div>
+      <div className='w-screen relative left-1/2 mx-[-50vw]'>
+        <div className='flex'>
+          <div className='flex flex-2 h-2 bg-gray-300 mt-3' />
+          {introductionItemProps.map((item) => (
+            <div key={item.description} className='basis-[380px] max-w-[380px] flex-col'>
+              <div className='flex items-center mb-4'>
+                <div className='w-4 h-2 bg-gray-300' />
+                <div className='w-8 h-8 border-4 border-gray-300 rounded-full' />
+                <div className='flex flex-1 h-2 bg-gray-300' />
+              </div>
+              <div className='text-lg text-gray-500 ml-4 mb-1'>{item.period}</div>
+              <div className='text-2xl font-semibold ml-4'>{item.description}</div>
+            </div>
+          ))}
+          <div className='flex flex-1 h-2 bg-gray-300 mt-3' />
         </div>
       </div>
     </div>
