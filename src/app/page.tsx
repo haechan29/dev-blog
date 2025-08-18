beb6c7115ec7228441c27896f7d6e35611ab5f66
haechan29 <haechan.im@gmail.com>
2025-08-18 17:46:00 +0900

    hide hearder section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import ProjectsItem from '@/components/projectsItem';
 export default function HomePage() {
   return (
     <div className='min-h-screen bg-white'>
+      {/* <header className='sticky top-0 z-40 border-b-gray-200 border-b bg-white/80 backdrop-blur'>
         <div className='mx-auto max-w-6xl px-4 py-3 flex gap-20 font-semibold'>
           <div>홈</div>
           <div>포트폴리오</div>
           <div>블로그</div>
         </div>
+      </header> */}
 
       <div className='mx-auto max-w-6xl px-4 flex flex-col'>
         <IntroductionItem className='mb-20' />
