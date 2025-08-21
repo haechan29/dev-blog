45bc8b9dc31ead9b99286eb8b36ab124b6b16b24
haechan29 <haechan.im@gmail.com>
2025-08-22 08:57:54 +0900

    create blog page

---
+++ src/features/post/ui/postItemProps.ts
@@ -0,0 +1,8 @@
+export type PostItemProps = {
+  slug: string;
+  title: string;
+  date: string;
+  tags: string[];
+  content: string;
+  plainText: string;
+}
---
+++ src/lib/posts.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import fs from 'fs';
+import matter from 'gray-matter';
+import { Post } from '@/features/post/domain/post';
+
+const postsDirectory = path.join(process.cwd(), 'src/posts');
+
+export function getPostBySlug(slug: string): Post {
+  const fullPath = path.join(postsDirectory, `${slug}.mdx`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const { data, content } = matter(fileContents);
+
+  console.log(data.title);
+
+  return new Post(
+    slug,
+    data.title,
+    data.date,
+    content,
+    data.tags
+  )
+}
+
+export function getAllPosts() {
+  const files = fs.readdirSync(postsDirectory);
+  const posts = files
+    .filter((file) => file.endsWith('.mdx'))
+    .map((file) => {
+      const slug = file.replace(/\.mdx$/, '');
+      return getPostBySlug(slug);
+    })
+  return posts.sort((a, b) => a.date > b.date ? -1 : 1);
+}
