bf8c903d03cb9e1834cadc705596814baf26ab95
haechan29 <haechan.im@gmail.com>
2025-08-25 13:00:51 +0900

    add mobile responsive design to toc item

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { getPostBySlug } from '@/lib/posts';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import rehypeSlug from 'rehype-slug';
 import rehypePrettyCode from 'rehype-pretty-code';
-import TableOfContents from '@/components/tableOfContentsItem';
+import TableOfContentsItem from '@/components/tableOfContentsItem';
 import ToggleButtonItem from '@/components/toggleButtonItem';
 import { ReactNode } from 'react';
 
@@ -16,7 +16,6 @@ export default async function PostPage({ params }: { params: Promise<{ slug: str
 
   return (
     <div className='max-w-2xl mx-auto px-4 py-20'>
-      {post.headings.length > 0 && (<TableOfContents headings={post.headings}/>)}
       <div className='text-4xl font-bold mb-4'>{post.title}</div>
       <div className='flex mb-10'>
         <div className='text-gray-500 mr-4'>{post.date}</div>
@@ -25,6 +24,12 @@ export default async function PostPage({ params }: { params: Promise<{ slug: str
         )}
       </div>
       <div className='w-full h-[1px] bg-gray-200 mb-10'/>
+      {post.headings.length > 0 && (
+        <div className='mb-10 xl:mb-0'>
+          <div className='block xl:hidden text-2xl font-bold text-gray-900 mt-4 mb-2 leading-tight'>목차</div>
+          <TableOfContentsItem headings={post.headings}/>
+        </div>
+      )}
       <div className='prose'>
         <MDXRemote
           source={post.content}
