96dc6c8f9ffec5a7211d0046fb3dd425a195f956
haechan29 <haechan.im@gmail.com>
2025-09-02 17:20:19 +0900

    improve post preview UI

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,32 +1,23 @@
+import PostPreviewItem from '@/components/postPreviewItem';
 import { fetchAllPosts } from '@/features/post/domain/service/postService';
-import Link from 'next/link';
 
-export default async function BlogPage({ searchParams }: { searchParams: Promise<{ tag?: string }> }) {
+export default async function BlogPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ tag?: string }>;
+}) {
   const tag = await searchParams.then(param => param.tag ?? null);
   const posts = await fetchAllPosts();
-  const filteredPosts = tag ? posts.filter(post => post.tags?.includes(tag)) : posts;
+  const filteredPosts = tag
+    ? posts.filter(post => post.tags?.includes(tag))
+    : posts;
   const postProps = filteredPosts.map(post => post.toProps());
 
   return (
-    <>
-      <div className='py-8 flex flex-col'>
-        {postProps.map((post) => (
-          <Link
-            key={post.slug} 
-            href={`/posts/${post.slug}`}
-            className='w-full py-8 border-b border-b-gray-200'
-          >
-            <div className='text-xl font-semibold text-black mb-4 truncate'>{post.title}</div>
-            <div className='text-sm text-gray-500 mb-4'>{post.date}</div>
-            <div className='text-black line-clamp-3 mb-4'>{post.plainText}</div>
-            <div className='flex flex-wrap gap-x-2'>
-              {post.tags.map(tag => (
-                <div key={tag} className='text-xs px-2 py-1 border border-gray-300 rounded-full'>{tag}</div>
-              ))}
-            </div>
-          </Link>
-        ))}
-      </div>
-    </>
+    <div className='py-8 flex flex-col'>
+      {postProps.map(post => (
+        <PostPreviewItem key={post.slug} post={post} />
+      ))}
+    </div>
   );
 }
---
+++ src/components/postPreviewItem.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { PostItemProps } from '@/features/post/ui/postItemProps';
+import clsx from 'clsx';
+import { Calendar, Heart, MessageCircle, Tag } from 'lucide-react';
+import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
+
+export default function PostPreviewItem({ post }: { post: PostItemProps }) {
+  const [isTextAreaExpanded, setIsTextAreaExpanded] = useState(false);
+  const timerRef = useRef<number | undefined>(undefined);
+
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div className={clsx('w-full py-8 border-b border-b-gray-200')}>
+      <Link
+        href={`/posts/${post.slug}`}
+        onMouseEnter={() => {
+          if (timerRef.current) {
+            clearTimeout(timerRef.current);
+          }
+          timerRef.current = window.setTimeout(() => {
+            setIsTextAreaExpanded(true);
+          }, 500);
+        }}
+        onMouseLeave={() => {
+          if (timerRef.current) {
+            clearTimeout(timerRef.current);
+            timerRef.current = undefined;
+          }
+          setIsTextAreaExpanded(false);
+        }}
+      >
+        <div className='text-xl font-semibold text-gray-900 mb-4 truncate'>
+          {post.title}
+        </div>
+        <div className='flex items-center text-sm text-gray-500 mb-4'>
+          <Calendar className='w-4 h-4 mr-2' />
+          {post.date}
+        </div>
+        <div
+          className={clsx(
+            'mb-4 overflow-hidden break-keep transition-discrete duration-500 leading-6 ',
+            'bg-gradient-to-b from-black via-black bg-clip-text text-transparent',
+            isTextAreaExpanded
+              ? 'max-h-[432px] to-white '
+              : 'max-h-[72px] to-black'
+          )}
+        >
+          {post.plainText}
+        </div>
+      </Link>
+
+      <div className='flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4'>
+        <div className='flex items-center gap-4'>
+          <div className='flex items-center gap-1 text-gray-500'>
+            <Heart className='w-4 h-4' />
+            <span className='text-sm'>{3}</span>
+          </div>
+          <div className='flex items-center gap-1 text-gray-500'>
+            <MessageCircle className='w-4 h-4' />
+            <span className='text-sm'>{4}</span>
+          </div>
+        </div>
+
+        <div className='flex items-center gap-2'>
+          <Tag className='w-4 h-4 text-gray-400' />
+          <div className='flex flex-wrap gap-2'>
+            {post.tags.map(tag => (
+              <div
+                key={tag}
+                className='text-xs px-2 py-1 border border-gray-300 rounded-full'
+              >
+                {tag}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
