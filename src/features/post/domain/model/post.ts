4cb36c4efbea868b25ddc8af76b77137759b6a71
haechan29 <haechan.im@gmail.com>
2025-09-12 10:43:03 +0900

    improve date format of posts

--- src/features/post/domain/model/post.ts
+++ src/features/post/domain/model/post.ts
@@ -1,14 +1,14 @@
-import { PostItemProps } from '@/features/post/ui/postItemProps'
+import { PostItemProps } from '@/features/post/ui/postItemProps';
 import { extractHeadings, extractPlainText } from '@/lib/posts';
 
 export type Heading = {
   id: string;
   text: string;
   level: number;
-}
+};
 
 export class Post {
-  constructor (
+  constructor(
     public readonly slug: string,
     public readonly title: string,
     public readonly date: string,
@@ -20,11 +20,35 @@ export class Post {
     return {
       slug: this.slug,
       title: this.title,
-      date: this.date,
+      date: formatDate(this.date),
       tags: this.tags ?? [],
       content: this.content,
       headings: extractHeadings(this.content),
-      plainText: extractPlainText(this.content)
+      plainText: extractPlainText(this.content),
     };
   }
+}
+
+function formatDate(dateStr: string): string {
+  const now = new Date();
+  const pDate = new Date(dateStr);
+  const timeDiff = now.getTime() - pDate.getTime();
+
+  const [year, month, date] = [
+    pDate.getFullYear(),
+    pDate.getMonth() + 1,
+    pDate.getDate(),
+  ];
+
+  if (now.getFullYear() !== year) return `${year}년 ${month}월 ${date}일`;
+
+  const diffInDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  if (diffInDays > 7) return `${month}월 ${date}일`;
+  if (diffInDays > 1) return `${diffInDays}일 전`;
+  if (diffInDays == 1) return '어제';
+
+  const diffInHours = Math.floor(timeDiff / (1000 * 60 * 60));
+  if (diffInHours >= 1) return `${diffInHours}시간 전`;
+
+  return '방금 전';
 }
