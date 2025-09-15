b22f25fa030193af283c79f2d09e81e52be98270
haechan29 <haechan.im@gmail.com>
2025-09-15 15:09:52 +0900

    add expand button on toolbar

--- src/components/postToolbar.tsx
+++ src/components/postToolbar.tsx
@@ -1,58 +1,78 @@
 'use client';
 
 import { toProps } from '@/features/post/domain/model/postToolbar';
-import { RootState } from '@/lib/redux/store';
+import { setType } from '@/lib/redux/postToolbarSlice';
+import { AppDispatch, RootState } from '@/lib/redux/store';
 import clsx from 'clsx';
-import { ChevronRight, Menu } from 'lucide-react';
+import { ChevronDown, ChevronRight, Menu } from 'lucide-react';
 import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function PostToolbar({ className }: { className?: string }) {
   return (
     <div className={className}>
-      <div
-        className={
-          'fixed top-0 left-0 right-0 pt-1 pb-4 pr-10 backdrop-blur-md bg-white/80 flex items-center'
-        }
-      >
-        <button className='flex px-2 place-self-end items-center justify-center'>
+      <div className='fixed top-0 left-0 right-0 pt-1 pb-4 backdrop-blur-md bg-white/80 flex items-end'>
+        <button className='flex px-2 items-center justify-center'>
           <Menu className='w-6 h-6' />
         </button>
-        <ContentItem />
+        <ContentItem className='flex flex-1 items-end' />
       </div>
     </div>
   );
 }
 
-function ContentItem() {
+function ContentItem({ className }: { className?: string }) {
   const postToolbar = useSelector((state: RootState) => state.postToolbar);
   const postToolbarProps = useMemo(() => toProps(postToolbar), [postToolbar]);
 
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
-    <div
-      className={clsx(
-        'flex flex-1 overflow-hidden transition-opacity duration-300 ease-in-out',
-        postToolbarProps.type === 'empty' ? 'opacity-0' : 'opacity-100'
-      )}
-    >
-      <div className='flex flex-col'>
-        <div className='flex text-xs h-4 text-gray-400'>
-          {(postToolbarProps.type === 'basic' ||
-            postToolbarProps.type === 'collapsed' ||
-            postToolbarProps.type === 'expanded') &&
-            postToolbarProps.breadcrumb.map(item => {
-              return (
-                <div key={item} className='flex items-center'>
-                  <div>{item}</div>
-                  <ChevronRight className='w-3 h-3' />
-                </div>
-              );
-            })}
-        </div>
-        <div className='font-semibold h-6 truncate'>
-          {postToolbarProps.type !== 'empty' && postToolbarProps.title}
+    <div className={className}>
+      <div className='flex flex-1'>
+        <div
+          className={clsx(
+            'flex flex-col overflow-hidden transition-opacity duration-300 ease-in-out',
+            postToolbarProps.type === 'empty' ? 'opacity-0' : 'opacity-100'
+          )}
+        >
+          <div className='flex text-xs h-4 text-gray-400'>
+            {(postToolbarProps.type === 'basic' ||
+              postToolbarProps.type === 'collapsed' ||
+              postToolbarProps.type === 'expanded') &&
+              postToolbarProps.breadcrumb.map(item => {
+                return (
+                  <div key={item} className='flex items-center'>
+                    <div>{item}</div>
+                    <ChevronRight className='w-3 h-3' />
+                  </div>
+                );
+              })}
+          </div>
+          <div className='font-semibold h-6 truncate'>
+            {postToolbarProps.type !== 'empty' && postToolbarProps.title}
+          </div>
         </div>
       </div>
+      <button
+        onClick={() =>
+          dispatch(
+            setType(
+              postToolbarProps.type === 'expanded' ? 'collapsed' : 'expanded'
+            )
+          )
+        }
+        className={clsx(
+          'flex px-2 items-center justify-center transition-opacity|transform duration-300 ease-in-out',
+          postToolbarProps.type === 'collapsed' ||
+            postToolbarProps.type === 'expanded'
+            ? 'opacity-100'
+            : 'opacity-0',
+          postToolbarProps.type === 'expanded' && '-rotate-180'
+        )}
+      >
+        <ChevronDown className='w-6 h-6' />
+      </button>
     </div>
   );
 }
