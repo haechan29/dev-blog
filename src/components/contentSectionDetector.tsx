5276f5950957092b8a269409031482f58f1857c7
haechan29 <haechan.im@gmail.com>
2025-09-15 14:03:09 +0900

    prevent first observation on prose section

--- src/components/contentSectionDetector.tsx
+++ src/components/contentSectionDetector.tsx
@@ -1,20 +1,26 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { setType } from '@/lib/redux/postToolbarSlice';
 import { AppDispatch } from '@/lib/redux/store';
 import { useDispatch } from 'react-redux';
 
 export default function ContentSectionDetector() {
   const dispatch = useDispatch<AppDispatch>();
+  const isInitialMount = useRef(true);
 
   useEffect(() => {
     const proseElement = document.querySelector('.prose');
     if (!proseElement) return;
 
     const proseObserver = new IntersectionObserver(
-      entries =>
-        dispatch(setType(entries[0].isIntersecting ? 'collapsed' : 'basic')),
+      entries => {
+        if (isInitialMount.current) {
+          isInitialMount.current = false;
+          return;
+        }
+        dispatch(setType(entries[0].isIntersecting ? 'collapsed' : 'basic'));
+      },
       {
         rootMargin: '-20px 0px -100% 0px',
       }
