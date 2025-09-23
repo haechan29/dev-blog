ff72a555ea2fc237fd7f56875ee5ee9dbb4861e8
haechan29 <haechan.im@gmail.com>
2025-09-23 17:57:44 +0900

    improve component parsing on fullscreen

--- src/components/post/postViewerContent.tsx
+++ src/components/post/postViewerContent.tsx
@@ -29,26 +29,28 @@ export default function PostViewerContent({
     let currentHeight = 0;
 
     children.forEach(child => {
-      const rect = child.getBoundingClientRect();
-      const style = window.getComputedStyle(child);
-      const marginTop = parseFloat(style.marginTop) || 0;
-      const marginBottom = parseFloat(style.marginBottom) || 0;
-      const elementHeight = rect.height + marginTop + marginBottom;
+      const isHeadingElement = child.matches('h1, h2, h3, h4, h5, h6');
+      const hasContent =
+        currentPage.length > 0 && !currentPage.every(isEmptyContent);
 
-      if (
-        currentHeight + elementHeight > window.screen.height - 80 &&
-        currentPage.length > 0
-      ) {
+      const elementHeight = getElementHeight(child);
+      const exceedsPageHeight =
+        currentHeight + elementHeight > window.screen.height - 80;
+
+      if ((isHeadingElement || exceedsPageHeight) && hasContent) {
         pages.push([...currentPage]);
-        currentPage = [child];
-        currentHeight = elementHeight;
-      } else {
+        currentPage = [];
+        currentHeight = 0;
+      }
+
+      if (!isHeadingElement) {
         currentPage.push(child);
         currentHeight += elementHeight;
       }
     });
 
-    if (currentPage.length > 0) {
+    const hasContent = !currentPage.every(isEmptyContent);
+    if (hasContent) {
       pages.push(currentPage);
     }
 
@@ -86,4 +88,26 @@ export default function PostViewerContent({
       )}
     />
   );
+}
+
+function getElementHeight(element: Element) {
+  const rect = element.getBoundingClientRect();
+  const style = window.getComputedStyle(element);
+  const marginTop = parseFloat(style.marginTop) || 0;
+  const marginBottom = parseFloat(style.marginBottom) || 0;
+  return rect.height + marginTop + marginBottom;
+}
+
+function isEmptyContent(element: Element) {
+  if (element.matches('br')) return true;
+
+  if (
+    element.matches('div, span, p') &&
+    element.textContent.trim() === '' &&
+    element.children.length === 0
+  ) {
+    return true;
+  }
+
+  return false;
 }
