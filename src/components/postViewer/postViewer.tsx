d7522f1c8f49be66e81dea7c0368112b9114e1ae
haechan29 <haechan.im@gmail.com>
2025-09-25 16:10:23 +0900

    refactor post viewer

--- src/components/postViewer/postViewer.tsx
+++ src/components/postViewer/postViewer.tsx
@@ -2,6 +2,7 @@
 
 import PostViewerContent from '@/components/postViewer/postViewerContent';
 import PostViewerControlBar from '@/components/postViewer/postViewerControlBar';
+import { toProps } from '@/features/postViewer/domain/model/postViewer';
 import useDebounce from '@/hooks/useDebounce';
 import useThrottle from '@/hooks/useThrottle';
 import {
@@ -12,7 +13,7 @@ import {
 } from '@/lib/redux/postViewerSlice';
 import { AppDispatch, RootState } from '@/lib/redux/store';
 import clsx from 'clsx';
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -21,47 +22,33 @@ export default function PostViewer() {
 
   const dispatch = useDispatch<AppDispatch>();
   const postViewer = useSelector((state: RootState) => state.postViewer);
+  const { isViewerMode, pageIndex, totalPages } = useMemo(
+    () => toProps(postViewer),
+    [postViewer]
+  );
   const postViewerRef = useRef<HTMLDivElement | null>(null);
 
   const debounce = useDebounce();
   const throttle = useThrottle();
 
-  useEffect(() => {
-    if (typeof document === 'undefined' || !postViewerRef.current) return;
-
-    if (postViewer.isViewerMode) {
-      postViewerRef.current.requestFullscreen();
-    } else if (document.fullscreenElement) {
-      document.exitFullscreen();
+  const handleFullscreenChange = useCallback(() => {
+    if (!document.fullscreenElement && isViewerMode) {
+      dispatch(setIsViewerMode(false));
     }
-  }, [postViewer.isViewerMode]);
+  }, [dispatch, isViewerMode]);
 
-  useEffect(() => {
-    if (typeof document === 'undefined') return;
+  const handleMouseMove = useCallback(() => {
+    throttle(() => {
+      dispatch(setIsControlBarVisible(true));
+      debounce(() => dispatch(setIsControlBarVisible(false)), 3000);
+    }, 100);
+  }, [debounce, dispatch, throttle]);
 
-    const handleFullscreenChange = () => {
-      if (!document.fullscreenElement && postViewer.isViewerMode) {
-        dispatch(setIsViewerMode(false));
-      }
-    };
-
-    document.addEventListener('fullscreenchange', handleFullscreenChange);
-    return () =>
-      document.removeEventListener('fullscreenchange', handleFullscreenChange);
-  }, [dispatch, postViewer.isViewerMode]);
-
-  useEffect(() => {
-    const handleMouseMove = () => {
-      throttle(() => {
-        dispatch(setIsControlBarVisible(true));
-        debounce(() => dispatch(setIsControlBarVisible(false)), 3000);
-      }, 100);
-    };
-
-    const handleScroll = (event: WheelEvent) => {
+  const handleScroll = useCallback(
+    (event: WheelEvent) => {
       throttle(() => {
         if (event.deltaY > 0) {
-          if (postViewer.pageIndex == postViewer.totalPages - 1) {
+          if (pageIndex == totalPages - 1) {
             toast.success('마지막 페이지입니다.', {
               id: 'post-viewer',
               toasterId: 'post-viewer',
@@ -70,7 +57,7 @@ export default function PostViewer() {
           }
           dispatch(nextPage());
         } else if (event.deltaY < 0) {
-          if (postViewer.pageIndex == 0) {
+          if (pageIndex == 0) {
             toast.success('첫 페이지입니다.', {
               id: 'post-viewer',
               toasterId: 'post-viewer',
@@ -80,9 +67,30 @@ export default function PostViewer() {
           dispatch(previousPage());
         }
       }, 100);
-    };
+    },
+    [dispatch, pageIndex, throttle, totalPages]
+  );
 
-    if (postViewer.isViewerMode) {
+  useEffect(() => {
+    if (typeof document === 'undefined' || !postViewerRef.current) return;
+
+    if (isViewerMode) {
+      postViewerRef.current.requestFullscreen();
+    } else if (document.fullscreenElement) {
+      document.exitFullscreen();
+    }
+  }, [isViewerMode]);
+
+  useEffect(() => {
+    if (typeof document === 'undefined') return;
+
+    document.addEventListener('fullscreenchange', handleFullscreenChange);
+    return () =>
+      document.removeEventListener('fullscreenchange', handleFullscreenChange);
+  }, [handleFullscreenChange]);
+
+  useEffect(() => {
+    if (isViewerMode) {
       document.addEventListener('mousemove', handleMouseMove);
       document.addEventListener('wheel', handleScroll);
     }
@@ -90,22 +98,12 @@ export default function PostViewer() {
       document.removeEventListener('mousemove', handleMouseMove);
       document.removeEventListener('wheel', handleScroll);
     };
-  }, [
-    dispatch,
-    postViewer.pageIndex,
-    postViewer.isViewerMode,
-    postViewer.totalPages,
-    throttle,
-    debounce,
-  ]);
+  }, [handleMouseMove, handleScroll, isViewerMode]);
 
   return (
     <div
       ref={postViewerRef}
-      className={clsx(
-        'bg-white flex flex-col',
-        !postViewer.isViewerMode && 'hidden'
-      )}
+      className={clsx('bg-white flex flex-col', !isViewerMode && 'hidden')}
     >
       <PostViewerContent pageRef={pageRef} />
       <PostViewerControlBar pageRef={pageRef} />
