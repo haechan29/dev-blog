3e0cebd57c283098b84780879e776dd7ca3ca2cb
haechan29 <haechan.im@gmail.com>
2025-10-01 15:40:06 +0900

    sync scroll position and page

--- src/components/post/postPageClient.tsx
+++ src/components/post/postPageClient.tsx
@@ -3,6 +3,7 @@
 import useContentTracker from '@/features/post/hooks/useContentTracker';
 import useHeadingTracker from '@/features/post/hooks/useHeadingTracker';
 import usePostToolbarSync from '@/features/post/hooks/usePostToolbarSync';
+import useScrollOnHeadingChange from '@/features/post/hooks/useScrollOnHeadingChange';
 import useViewTracker from '@/features/post/hooks/useViewTracker';
 import { PostProps } from '@/features/post/ui/postProps';
 
@@ -11,6 +12,7 @@ export default function PostPageClient({ post }: { post: PostProps }) {
   useHeadingTracker(post);
   usePostToolbarSync(post);
   useViewTracker(post);
+  useScrollOnHeadingChange();
 
   return null;
 }
