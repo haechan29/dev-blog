4b69e37e033efc6bd0113340e4171a7b3198c2c4
haechan29 <haechan.im@gmail.com>
2025-10-06 21:14:25 +0900

    refactor postSidebar

--- src/components/post/postSidebar.tsx
+++ src/components/post/postSidebar.tsx
@@ -1,25 +1,24 @@
 'use client';
 
-import PostSidebarFooterSection from '@/components/post/postSidebarFooterSection';
-import useSidebar from '@/features/post/hooks/useSidebar';
+import PostSidebarFooter from '@/components/post/postSidebarFooter';
+import PostSidebarHeader from '@/components/post/postSidebarHeader';
+import PostSidebarNav from '@/components/post/postSidebarNav';
 import useSidebarSwipe from '@/features/post/hooks/useSidebarSwipe';
 import { PostProps } from '@/features/post/ui/postProps';
-import { setIsVisible } from '@/lib/redux/postSidebarSlice';
-import { AppDispatch } from '@/lib/redux/store';
+import useMediaQuery from '@/hooks/useMediaQuery';
+import { RootState } from '@/lib/redux/store';
 import clsx from 'clsx';
-import Link from 'next/link';
-import { useParams } from 'next/navigation';
-import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 
 export default function PostSidebar({ posts }: { posts: PostProps[] }) {
-  const params = useParams();
-  const selectedSlug = params.slug as string | undefined;
+  const postSidebar = useSelector((state: RootState) => state.postSidebar);
+  const isLargerThanXl = useMediaQuery('(min-width: 1280px)');
 
-  const dispatch = useDispatch<AppDispatch>();
-
-  const { selectedTag, tagCount, postsOfTag, isVisible } = useSidebar({
-    posts,
-  });
+  const isVisible = useMemo(
+    () => isLargerThanXl || postSidebar.isVisible,
+    [postSidebar, isLargerThanXl]
+  );
 
   const swipeHandlers = useSidebarSwipe();
 
@@ -32,70 +31,9 @@ export default function PostSidebar({ posts }: { posts: PostProps[] }) {
       )}
     >
       <div className='flex flex-col w-full min-w-0 h-dvh bg-[#fafbfc] border-r border-r-gray-50'>
-        <div className='flex w-full min-w-0 px-6 py-9'>
-          <Link
-            onClick={() => dispatch(setIsVisible(false))}
-            className='flex flex-col min-w-0 px-3 py-3'
-            href='/posts'
-          >
-            <div className='text-2xl font-bold tracking-tight text-blue-500'>
-              Haechan
-            </div>
-            <div className='text-xs text-gray-400 mt-1 font-light tracking-wide'>
-              DEV BLOG
-            </div>
-          </Link>
-        </div>
-
-        <div className='flex flex-col flex-1 overflow-y-auto'>
-          {tagCount.map(([tag, count]) => {
-            return (
-              <div key={tag}>
-                <Link
-                  href={
-                    !selectedSlug && selectedTag === tag
-                      ? '/posts'
-                      : `/posts?tag=${tag}`
-                  }
-                  className={clsx(
-                    'flex items-center w-full py-3 px-9 gap-2 hover:text-blue-500',
-                    !selectedSlug && selectedTag === tag
-                      ? 'bg-blue-50 font-semibold text-blue-500'
-                      : 'text-gray-900'
-                  )}
-                >
-                  <div className='flex-1 text-sm'>{tag}</div>
-                  {selectedTag !== tag && (
-                    <div className='flex-shrink-0 text-xs text-gray-400'>
-                      {count}
-                    </div>
-                  )}
-                </Link>
-                {selectedTag === tag &&
-                  postsOfTag !== null &&
-                  postsOfTag.map(post => (
-                    <Link
-                      key={`${tag}-${post.slug} `}
-                      href={`/posts/${post.slug}${
-                        selectedTag ? `?tag=${selectedTag}` : ''
-                      }`}
-                      onClick={() => dispatch(setIsVisible(false))}
-                      className={clsx(
-                        'flex w-full py-3 pl-12 pr-9 hover:text-blue-500',
-                        post.slug === selectedSlug
-                          ? 'bg-blue-50 font-semibold text-blue-500'
-                          : 'text-gray-900'
-                      )}
-                    >
-                      <div className='text-sm'>{post.title}</div>
-                    </Link>
-                  ))}
-              </div>
-            );
-          })}
-        </div>
-
-        <PostSidebarFooterSection />
+        <PostSidebarHeader />
+        <PostSidebarNav posts={posts} />
+        <PostSidebarFooter />
       </div>
     </div>
   );
---
+++ src/components/post/postSidebarNav.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import Post from '@/features/post/domain/model/post';
+import { PostProps } from '@/features/post/ui/postProps';
+import { setIsVisible } from '@/lib/redux/postSidebarSlice';
+import { AppDispatch } from '@/lib/redux/store';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { useParams, useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+
+export default function PostSidebarNav({ posts }: { posts: PostProps[] }) {
+  const tagCount = useMemo(() => getTagCounts(posts), [posts]);
+
+  return (
+    <div className='flex flex-col flex-1 overflow-y-auto'>
+      {tagCount.map(([tag, count]) => {
+        return (
+          <div key={tag}>
+            <NavCategory tag={tag} count={count} />
+            <NavPostList tag={tag} posts={posts} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function NavCategory({ tag, count }: { tag: string; count: number }) {
+  const searchParams = useSearchParams();
+  const currentTag = searchParams.get('tag') ?? null;
+
+  const params = useParams();
+  const currentSlug = params.slug as string | undefined;
+
+  const isTagSelected = useMemo(() => currentTag === tag, [currentTag, tag]);
+
+  const categoryUrl = useMemo(() => {
+    let categoryUrl = '/posts';
+    if (currentSlug || !isTagSelected) categoryUrl += `?tag=${tag}`;
+    return categoryUrl;
+  }, [currentSlug, isTagSelected, tag]);
+
+  return (
+    <Link
+      href={categoryUrl}
+      className={clsx(
+        'flex items-center w-full py-3 px-9 gap-2 hover:text-blue-500',
+        !currentSlug && isTagSelected
+          ? 'bg-blue-50 font-semibold text-blue-500'
+          : 'text-gray-900'
+      )}
+    >
+      <div className='flex-1 text-sm'>{tag}</div>
+      {!isTagSelected && (
+        <div className='flex-shrink-0 text-xs text-gray-400'>{count}</div>
+      )}
+    </Link>
+  );
+}
+
+function NavPostList({ tag, posts }: { tag: string; posts: PostProps[] }) {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const searchParams = useSearchParams();
+  const currentTag = searchParams.get('tag') ?? null;
+
+  const params = useParams();
+  const currentSlug = params.slug as string | undefined;
+
+  const postsOfTag = useMemo(() => {
+    return currentTag
+      ? posts.filter(post => post.tags.includes(currentTag))
+      : [];
+  }, [posts, currentTag]);
+
+  const isTagSelected = useMemo(() => currentTag === tag, [currentTag, tag]);
+
+  return (
+    isTagSelected &&
+    postsOfTag.map(post => {
+      const postUrl = getPostUrl(currentTag, post);
+      const isSlugSelected = post.slug === currentSlug;
+
+      return (
+        <Link
+          key={`${tag}-${post.slug} `}
+          href={postUrl}
+          onClick={() => dispatch(setIsVisible(false))}
+          className={clsx(
+            'flex w-full py-3 pl-12 pr-9 hover:text-blue-500',
+            isSlugSelected
+              ? 'bg-blue-50 font-semibold text-blue-500'
+              : 'text-gray-900'
+          )}
+        >
+          <div className='text-sm'>{post.title}</div>
+        </Link>
+      );
+    })
+  );
+}
+
+function getTagCounts(posts: PostProps[]) {
+  const tagMap = posts
+    .flatMap(post => post.tags)
+    .reduce(
+      (acc, tag) => acc.set(tag, (acc.get(tag) ?? 0) + 1),
+      new Map<string, number>()
+    );
+  return [...tagMap.entries()];
+}
+
+function getPostUrl(tag: string | null, post: Post) {
+  let postUrl = `/posts/${post.slug}`;
+  if (tag) postUrl += `?tag=${tag}`;
+  return postUrl;
+}
