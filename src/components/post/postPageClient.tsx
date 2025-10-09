5aa5e00a31ad412ee6c2abc48084f7bb7c8585e9
haechan29 <haechan.im@gmail.com>
2025-10-09 18:40:40 +0900

    lock scroll on viewer mode

--- src/components/post/postPageClient.tsx
+++ src/components/post/postPageClient.tsx
@@ -6,13 +6,18 @@ import usePostToolbarSync from '@/features/post/hooks/usePostToolbarSync';
 import useScrollOnHeadingChange from '@/features/post/hooks/useScrollOnHeadingChange';
 import useViewTracker from '@/features/post/hooks/useViewTracker';
 import { PostProps } from '@/features/post/ui/postProps';
+import usePostViewer from '@/features/postViewer/hooks/usePostViewer';
+import useScrollLock from '@/hooks/useScrollLock';
 
 export default function PostPageClient({ post }: { post: PostProps }) {
+  const { isViewerMode } = usePostViewer();
+
   useContentTracker();
   useHeadingTracker(post);
   usePostToolbarSync(post);
   useViewTracker(post);
   useScrollOnHeadingChange();
+  useScrollLock(isViewerMode);
 
   return null;
 }
