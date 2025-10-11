86f7c697effb3cce2e1ffd8df38229e5213d42b4
haechan29 <haechan.im@gmail.com>
2025-10-11 21:46:05 +0900

    change highlight on active element

--- src/features/postViewer/hooks/useTTSPlayer.ts
+++ src/features/postViewer/hooks/useTTSPlayer.ts
@@ -15,20 +15,9 @@ export default function useTTSPlayer({
 }) {
   const isPaused = useRef<boolean | null>(null);
 
-  const startReading = useCallback(
+  const addHighlight = useCallback(
     (elementIndex: number) => {
       if (!readablePage) return;
-
-      if (isPaused.current) {
-        speechSynthesis.resume();
-        isPaused.current = false;
-        return;
-      }
-
-      if (elementIndex >= readablePage.length) {
-        onFinishPage();
-        return;
-      }
 
       readablePage.forEach((element, index) => {
         element.classList.remove('tts-reading-active', 'tts-reading-inactive');
@@ -39,6 +28,34 @@ export default function useTTSPlayer({
           element.classList.add('tts-reading-inactive');
         }
       });
+    },
+    [readablePage]
+  );
+
+  const clearHighlight = useCallback(() => {
+    if (readablePage) {
+      readablePage.forEach(element => {
+        element.classList.remove('tts-reading-active', 'tts-reading-inactive');
+      });
+    }
+  }, [readablePage]);
+
+  const startReading = useCallback(
+    (elementIndex: number) => {
+      if (!readablePage) return;
+
+      if (elementIndex >= readablePage.length) {
+        onFinishPage();
+        return;
+      }
+
+      addHighlight(elementIndex);
+
+      if (isPaused.current) {
+        speechSynthesis.resume();
+        isPaused.current = false;
+        return;
+      }
 
       const element = readablePage[elementIndex];
       const utterance = getUtterance(element.textContent);
@@ -48,25 +65,22 @@ export default function useTTSPlayer({
       speechSynthesis.speak(utterance);
       isPaused.current = false;
     },
-    [onFinishElement, onFinishPage, readablePage]
+    [addHighlight, onFinishElement, onFinishPage, readablePage]
   );
 
   const pauseReading = useCallback(() => {
     speechSynthesis.pause();
+    clearHighlight();
+
     if (isPaused.current === false) {
       isPaused.current = true;
     }
-  }, []);
+  }, [clearHighlight]);
 
   const stopReading = useCallback(() => {
     speechSynthesis.cancel();
-
-    if (readablePage) {
-      readablePage.forEach(element => {
-        element.classList.remove('tts-reading-active', 'tts-reading-inactive');
-      });
-    }
-  }, [readablePage]);
+    clearHighlight();
+  }, [clearHighlight]);
 
   return { startReading, pauseReading, stopReading } as const;
 }
