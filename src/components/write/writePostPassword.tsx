ce9df109354c6ca6193a97ad4a88759c09f67191
haechan29 <haechan.im@gmail.com>
2025-10-21 20:06:32 +0900

    add show password button

--- src/components/write/writePostPassword.tsx
+++ src/components/write/writePostPassword.tsx
@@ -3,7 +3,8 @@
 import { WritePostFormProps } from '@/features/write/ui/writePostFormProps';
 import { WritePostValidityProps } from '@/features/write/ui/writePostValidityProps';
 import clsx from 'clsx';
-import { ChangeEvent, useCallback, useMemo } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
+import { ChangeEvent, useCallback, useMemo, useState } from 'react';
 
 export default function WritePostPassword({
   writePostForm: { password },
@@ -16,6 +17,8 @@ export default function WritePostPassword({
   setPassword: (password: string) => void;
   setShouldValidate: (shouldValidate: boolean) => void;
 }) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
   const isInvalid = useMemo(() => invalidMeta === 'password', [invalidMeta]);
 
   const onChange = useCallback(
@@ -27,18 +30,37 @@ export default function WritePostPassword({
   );
 
   return (
-    <input
-      type='password'
-      value={password}
-      onChange={onChange}
-      placeholder='비밀번호'
+    <div
       className={clsx(
-        'w-full p-3 text-sm outline-none border rounded-lg',
+        'flex border rounded-lg hover:border-blue-500',
+        !isPasswordVisible && !password && 'bg-gray-50',
         isInvalid
           ? 'border-red-400 animate-shake'
-          : 'border-gray-200 hover:border-blue-500 focus:border-blue-500',
-        password || 'bg-gray-50'
+          : isFocused
+          ? 'border-blue-500'
+          : 'border-gray-200'
       )}
-    />
+    >
+      <input
+        type={isPasswordVisible ? 'text' : 'password'}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        value={password}
+        onChange={onChange}
+        placeholder='비밀번호'
+        className='flex-1 min-w-0 p-3 text-sm outline-none'
+      />
+
+      <button
+        onClick={() => setIsPasswordVisible(prev => !prev)}
+        className='flex shrink-0 p-2 mr-1 justify-center items-center'
+      >
+        {isPasswordVisible ? (
+          <EyeOff className='w-5 h-5' />
+        ) : (
+          <Eye className='w-5 h-5' />
+        )}
+      </button>
+    </div>
   );
 }
