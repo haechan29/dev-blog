f0cc6271ba6b8731632058f3e3c84c59e2cea5c9
haechan29 <haechan.im@gmail.com>
2025-10-24 13:47:32 +0900

    add too many tags responsive UI

--- src/components/write/writePostTag.tsx
+++ src/components/write/writePostTag.tsx
@@ -4,7 +4,7 @@ import useWritePostTag from '@/features/write/hooks/useWritePostTag';
 import { WritePostFormProps } from '@/features/write/ui/writePostFormProps';
 import { WritePostValidityProps } from '@/features/write/ui/writePostValidityProps';
 import clsx from 'clsx';
-import { ChangeEvent, useCallback, useMemo, useState } from 'react';
+import { ChangeEvent, useCallback, useMemo, useRef, useState } from 'react';
 
 export const MAX_TAG_LENGTH = 31;
 const MAX_TAGS_LENGTH = 10;
@@ -21,6 +21,7 @@ export default function WritePostTag({
   setShouldValidate: (shouldValidate: boolean) => void;
 }) {
   const [isFocused, setIsFocused] = useState(false);
+  const tagRef = useRef<HTMLInputElement | null>(null);
   const isInvalid = useMemo(() => invalidMeta === 'tags', [invalidMeta]);
   const { tag, isTagEmpty, insertTag, updateTag } = useWritePostTag({
     tags,
@@ -50,7 +51,7 @@ export default function WritePostTag({
   return (
     <div
       className={clsx(
-        'flex w-full border rounded-lg gap-2 items-center px-3',
+        'w-full flex border rounded-lg gap-2 items-center px-3',
         isInvalid || areTagsTooMany || isTagTooLong
           ? 'border-red-400 animate-shake'
           : isFocused
@@ -59,73 +60,82 @@ export default function WritePostTag({
         isTagEmpty && 'text-gray-400',
         tags.length === 0 && isTagEmpty && 'bg-gray-50'
       )}
+      onClick={() => tagRef.current?.focus()}
     >
-      {tags.map((tag, index) => (
+      <div className='flex flex-1 min-w-0 overflow-x-auto scrollbar-hide gap-2 items-center'>
+        {tags.map((tag, index) => (
+          <div
+            key={tag}
+            className={clsx(
+              'shrink-0 text-sm',
+              index < MAX_TAGS_LENGTH ? 'text-blue-500' : 'text-red-500'
+            )}
+          >
+            {tag}
+          </div>
+        ))}
+
         <div
-          key={tag}
           className={clsx(
-            'shrink-0 text-sm',
-            index < MAX_TAGS_LENGTH ? 'text-blue-500' : 'text-red-500'
+            'w-fit shrink-0 relative text-sm my-3',
+            !isFocused && tags.length > 0 && 'opacity-0'
           )}
         >
-          {tag}
-        </div>
-      ))}
+          <div className={clsx('inline-block', isTagEmpty && 'text-gray-400')}>
+            {isTagEmpty ? '#태그' : tag}
+          </div>
+          <input
+            ref={tagRef}
+            type='text'
+            onFocus={onFocus}
+            onBlur={onBlur}
+            value={tag}
+            onChange={onChange}
+            className='absolute z-50 inset-0 outline-none text-transparent caret-gray-900'
+          />
 
-      <div
-        className={clsx(
-          'relative flex flex-1 min-w-0 text-sm',
-          !isFocused && tags.length > 0 && 'opacity-0'
-        )}
-      >
-        <input
-          type='text'
-          onFocus={onFocus}
-          onBlur={onBlur}
-          value={tag}
-          onChange={onChange}
-          className={'py-3 flex-1 min-w-0 outline-none'}
-        />
-
-        {isTagEmpty && (
-          <div className='flex w-0 overflow-x-visible absolute left-0 inset-y-0 py-3 items-center text-gray-400'>
+          {/* {isTagEmpty && (
+          <div className='flex w-0 overflow-x-visible absolute z-50 left-0 inset-y-0 py-3 items-center text-gray-400'>
             <div className='shrink-0 invisible'>#</div>
             <div className='shrink-0'>태그</div>
           </div>
         )}
 
         {isTagTooLong && (
-          <div className='flex w-0 overflow-x-visible absolute left-0 inset-y-0 py-3 items-center text-red-500'>
+          <div className='flex w-0 overflow-x-visible absolute z-50 left-0 inset-y-0 py-3 items-center text-red-500'>
             <div className='shrink-0 invisible'>
               {tag.slice(0, MAX_TAG_LENGTH)}
             </div>
             <div className='shrink-0'>{tag.slice(MAX_TAG_LENGTH)}</div>
           </div>
-        )}
+        )} */}
+        </div>
       </div>
 
-      <div
-        className={clsx(
-          'flex text-sm gap-0.5 items-center',
-          isTagTooLong && 'text-red-500',
-          (!isFocused || tag.length < MAX_TAG_LENGTH * 0.95) && 'hidden'
-        )}
-      >
-        <div>{tag.length - 1}</div>
-        <div>/</div>
-        <div>{MAX_TAG_LENGTH - 1}</div>
-      </div>
+      <div className='shrink-0'>
+        <div
+          className={clsx(
+            'flex text-sm gap-0.5 items-center',
+            isTagTooLong && 'text-red-500',
+            (!isFocused || tag.length < MAX_TAG_LENGTH * 0.95) && 'hidden'
+          )}
+        >
+          <div>{tag.length - 1}</div>
+          <div>/</div>
+          <div>{MAX_TAG_LENGTH - 1}</div>
+        </div>
 
-      <div
-        className={clsx(
-          'flex text-sm gap-0.5 items-center',
-          areTagsTooMany ? 'text-red-500' : 'text-blue-500',
-          (tags.length === 0 || !isTagEmpty) && 'hidden'
-        )}
-      >
-        <div>{tags.length}</div>
-        <div>/</div>
-        <div>{MAX_TAGS_LENGTH}</div>
+        <div
+          className={clsx(
+            'flex text-sm gap-0.5 items-center',
+            areTagsTooMany ? 'text-red-500' : 'text-blue-500',
+            (tags.length === 0 || !isTagEmpty) && 'hidden'
+          )}
+        >
+          <div>{tags.length}</div>
+          <div>/</div>
+          <div>{MAX_TAGS_LENGTH}</div>
+        </div>
       </div>
     </div>
   );
