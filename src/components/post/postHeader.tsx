b852a6c13020e2a43e29fc1df245c2be0897dbfb
haechan29 <haechan.im@gmail.com>
2025-10-28 15:52:12 +0900

    add title and tags length limit

--- src/components/post/postHeader.tsx
+++ src/components/post/postHeader.tsx
@@ -1,33 +1,32 @@
 import PostInfo from '@/components/post/postInfo';
 import { PostProps } from '@/features/post/ui/postProps';
-import Link from 'next/link';
+import clsx from 'clsx';
 
 export default function PostHeader({ post }: { post: PostProps }) {
+  const { title, tags } = post;
   return (
-    <>
-      <Title {...post} />
-      <Tags {...post} />
+    <div className='flex flex-col gap-6'>
+      <div className='text-3xl font-bold line-clamp-2'>{title}</div>
+
+      <div className='flex'>
+        <div className='w-full flex overflow-x-auto scrollbar-hide'>
+          <div className='flex gap-3'>
+            {tags.map((tag, index) => (
+              <div
+                key={tag}
+                className={clsx(
+                  'text-center text-xs px-2 py-1 border border-gray-300 rounded-full',
+                  index >= 3 && 'max-w-20 truncate'
+                )}
+              >
+                {tag}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
       <PostInfo {...post} />
-    </>
-  );
-}
-
-function Title({ title }: { title: string }) {
-  return <div className='text-3xl font-bold mb-6'>{title}</div>;
-}
-
-function Tags({ tags }: { tags: string[] }) {
-  return (
-    <div className='flex flex-wrap gap-3 mb-6'>
-      {tags.map(tag => (
-        <Link
-          key={tag}
-          href={`/posts?tag=${tag}`}
-          className='text-xs px-2 py-1 border border-gray-300 rounded-full hover:text-blue-500 hover:border-blue-200'
-        >
-          {tag}
-        </Link>
-      ))}
     </div>
   );
 }
