9f37fa45b37c5a601b741757564e0c3347fd3f65
haechan29 <haechan.im@gmail.com>
2025-10-29 12:24:11 +0900

    add postSettingDropdown UI

--- src/components/post/postSettingsDropdown.tsx
+++ src/components/post/postSettingsDropdown.tsx
@@ -37,18 +37,18 @@ export default function PostSettingsDropdown({
       <DropdownMenuContent align='end'>
         <DropdownMenuItem
           onClick={onEdit}
-          className='flex items-center gap-2 text-gray-400 hover:text-blue-600 cursor-pointer'
+          className='w-full flex items-center gap-2 cursor-pointer'
         >
-          <Edit2 size={16} />
-          수정
+          <Edit2 className='w-4 h-4 text-gray-500' />
+          <div className='shrink-0 text-gray-900'>수정</div>
         </DropdownMenuItem>
 
         <DropdownMenuItem
           onClick={onDelete}
-          className='flex items-center gap-2 text-gray-400 hover:text-red-600 cursor-pointer'
+          className='w-full flex items-center gap-2 cursor-pointer'
         >
-          <Trash2 size={16} />
-          삭제
+          <Trash2 className='w-4 h-4 text-red-400' />
+          <div className='shrink-0 text-red-600'>삭제</div>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
