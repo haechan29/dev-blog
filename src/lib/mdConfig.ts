824adb284446189ea8dc1ff0c11902205e13cda0
haechan29 <haechan.im@gmail.com>
2025-11-02 11:50:32 +0900

    add image-container to remark image

--- src/lib/mdConfig.ts
+++ src/lib/mdConfig.ts
@@ -5,7 +5,7 @@ import type {
   TextDirective,
 } from 'mdast-util-directive';
 import { defaultSchema, type Options } from 'rehype-sanitize';
-import type { Node } from 'unist';
+import type { Node, Parent } from 'unist';
 import { visit } from 'unist-util-visit';
 
 type HtmlElement = {
@@ -33,12 +33,17 @@ export const schema: Options = {
       ...(defaultSchema.attributes?.img ?? []),
       ['className', 'w-full', 'min-w-56 w-1/2'],
     ],
+    div: [
+      ...(defaultSchema.attributes?.div ?? []),
+      ['className', 'image-container'],
+      'data-original-caption',
+    ],
   },
 };
 
 export function remarkCustomDirectives() {
   return (tree: Node) => {
-    visit(tree, node => {
+    visit(tree, (node: Node, index?: number, parent?: Parent) => {
       if (!isDirectiveNode(node)) return;
       if (customElements[node.name]) {
         const { name, properties } = customElements[node.name];
@@ -46,8 +51,8 @@ export function remarkCustomDirectives() {
           hName: name,
           hProperties: properties,
         };
-      } else if (node.name === 'img') {
-        remarkImage(node);
+      } else if (node.name === 'img' && index !== undefined && parent) {
+        remarkImage(node, index, parent);
       }
     });
   };
@@ -75,16 +80,63 @@ function isDirectiveNode(node: Node): node is DirectiveNode {
   );
 }
 
-function remarkImage(node: DirectiveNode) {
-  const { src, alt, size } = node.attributes || {};
-  if (!src) return;
+function remarkImage(node: DirectiveNode, index: number, parent: Parent) {
+  const { url, alt, size } = node.attributes || {};
+  if (!url) return;
 
-  node.data = {
-    hName: 'img',
-    hProperties: {
-      src,
-      ...(alt && { alt }),
-      className: size === 'large' ? 'w-full' : 'min-w-56 w-1/2',
+  const originalCaption = node.children
+    .filter(child => child.type === 'paragraph')
+    .map(p =>
+      p.children
+        .map(child => {
+          if (child.type === 'text') return child.value;
+          if (child.type === 'break') return '\n';
+          return '';
+        })
+        .join('')
+    )
+    .join('\n');
+
+  const caption = node.children.map(child => {
+    if (child.type === 'paragraph') {
+      return {
+        ...child,
+        children: child.children.map(textNode => {
+          if (textNode.type === 'text' && textNode.value) {
+            return {
+              ...textNode,
+              value: textNode.value.replace(/#/g, ''),
+            };
+          }
+          return textNode;
+        }),
+      };
+    }
+    return child;
+  });
+
+  const imageContainer = {
+    type: 'imageContainer',
+    data: {
+      hProperties: {
+        className: ['image-container'],
+        'data-original-caption': originalCaption,
+      },
     },
+    children: [
+      {
+        type: 'image',
+        url,
+        alt: alt || '',
+        data: {
+          hProperties: {
+            className: size === 'large' ? ['w-full'] : ['min-w-56 w-1/2'],
+          },
+        },
+      },
+      ...caption,
+    ],
   };
+
+  parent.children[index] = imageContainer;
 }
