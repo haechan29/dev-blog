a587296cc4b5eeec2b89a2db284fc977de45200d
haechan29 <haechan.im@gmail.com>
2025-11-18 10:23:40 +0900

    add time delay to fullscreen size measure

--- src/features/postViewer/hooks/useViewerContainerSize.ts
+++ src/features/postViewer/hooks/useViewerContainerSize.ts
@@ -4,10 +4,12 @@ import { supportsFullscreen } from '@/lib/browser';
 import { getCSSVariable } from '@/lib/css';
 import { remToPx } from '@/lib/dom';
 import { Size } from '@/types/size';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 export default function useViewerContainerSize() {
+  const windowSizeRef = useRef<Size | null>(null);
   const [fullscreenSize, setFullscreenSize] = useState<Size | null>(null);
+  const isFullscreenSizeSetRef = useRef(false);
 
   const containerSize = useMemo(() => {
     if (!fullscreenSize) return;
@@ -24,39 +26,52 @@ export default function useViewerContainerSize() {
     };
   }, [fullscreenSize]);
 
-  const handleFullscreenChange = useCallback(() => {
-    if (typeof document === 'undefined' || typeof window === 'undefined')
-      return;
-
-    if (!fullscreenSize && document.fullscreenElement) {
-      setFullscreenSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    }
-  }, [fullscreenSize]);
+  useEffect(() => {
+    windowSizeRef.current = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  }, []);
 
   useEffect(() => {
-    if (typeof document === 'undefined' || typeof window === 'undefined')
-      return;
-
+    if (isFullscreenSizeSetRef.current) return;
     if (supportsFullscreen) {
+      const handleFullscreenChange = () => {
+        if (!document.fullscreenElement || !windowSizeRef.current) return;
+        let retryCount = 0;
+        const maxCount = 10;
+        const { width, height } = windowSizeRef.current;
+
+        const interval = setInterval(() => {
+          const { innerWidth: newWidth, innerHeight: newHeight } = window;
+          if (width !== newWidth || height !== newHeight) {
+            setFullscreenSize({
+              width: newWidth,
+              height: newHeight,
+            });
+            isFullscreenSizeSetRef.current = true;
+            clearInterval(interval);
+          } else if (++retryCount >= maxCount) {
+            clearInterval(interval);
+          }
+        }, 100);
+      };
+
       document.addEventListener('fullscreenchange', handleFullscreenChange);
       return () =>
         document.removeEventListener(
           'fullscreenchange',
           handleFullscreenChange
         );
-    } else {
-      if (!fullscreenSize) {
-        // for 90° rotated layout: swap width/height since viewport dimensions are inverted
-        setFullscreenSize({
-          width: window.innerHeight,
-          height: window.innerWidth,
-        });
-      }
+    } else if (!supportsFullscreen) {
+      // for 90° rotated layout: swap width/height since viewport dimensions are inverted
+      setFullscreenSize({
+        width: window.innerHeight,
+        height: window.innerWidth,
+      });
+      isFullscreenSizeSetRef.current = true;
     }
-  }, [fullscreenSize, handleFullscreenChange]);
+  }, []);
 
   return containerSize;
 }
