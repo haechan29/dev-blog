94ea7443c1b9516a76faef7ad922aea222690282
haechan29 <haechan.im@gmail.com>
2025-11-26 11:20:07 +0900

    add supabase adapter

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,12 @@
+import { SupabaseAdapter } from '@auth/supabase-adapter';
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  }),
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
@@ -10,5 +15,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   pages: {
     signIn: '/login',
+    newUser: '/signup',
   },
 });
