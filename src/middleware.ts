124112e0429ca7108df7d0a73c4edae2be261b40
haechan29 <haechan.im@gmail.com>
2025-12-01 12:41:36 +0900

    add middleware setting for user page

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,16 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-  const session = await auth();
+  let response: NextResponse;
 
+  if (request.nextUrl.pathname.startsWith('/@')) {
+    const userId = request.nextUrl.pathname.slice(2);
+    response = NextResponse.rewrite(new URL(`/user/${userId}`, request.url));
+  } else {
+    response = NextResponse.next();
+  }
+
+  const session = await auth();
   if (session) return response;
 
   const guestId = request.cookies.get('guestId')?.value ?? nanoid();
