bc9e5761cb47b688d72a54b6f15919a6efa82eef
haechan29 <haechan.im@gmail.com>
2025-12-01 15:21:30 +0900

    refactor user tabs component

--- src/components/user/userTabs.tsx
+++ src/components/user/userTabs.tsx
@@ -1,36 +1,34 @@
 'use client';
 
+import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function UserNavTabs({ userId }: { userId: string }) {
   const pathname = usePathname();
-  const isPostsPage = pathname.includes('/posts');
-  const isSeriesPage = pathname.includes('/series');
+
+  const tabs = [
+    { key: 'posts', label: '글' },
+    { key: 'series', label: '시리즈' },
+  ];
 
   return (
     <div className='border-b border-gray-200'>
-      <div className='flex gap-8'>
-        <Link
-          href={`/@${userId}/posts`}
-          className={`pb-4 px-1 ${
-            isPostsPage
-              ? 'border-b-2 border-blue-500 text-blue-600'
-              : 'text-gray-500 hover:text-gray-700'
-          }`}
-        >
-          글
-        </Link>
-        <Link
-          href={`/@${userId}/series`}
-          className={`pb-4 px-1 ${
-            isSeriesPage
-              ? 'border-b-2 border-blue-500 text-blue-600'
-              : 'text-gray-500 hover:text-gray-700'
-          }`}
-        >
-          시리즈
-        </Link>
+      <div className='flex gap-6'>
+        {tabs.map(({ key, label }) => (
+          <Link
+            key={key}
+            href={`/@${userId}/${key}`}
+            className={clsx(
+              'py-3 px-1 min-w-12 text-center',
+              pathname.includes(`/${key}`)
+                ? 'border-b-2 border-gray-700 text-gray-900 font-semibold'
+                : 'hover:border-b-2 border-gray-300 text-gray-700'
+            )}
+          >
+            {label}
+          </Link>
+        ))}
       </div>
     </div>
   );
