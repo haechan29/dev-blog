b896c93846a4c0d75aba1b1833776ba8746eaf30
haechan29 <haechan.im@gmail.com>
2025-12-04 20:42:33 +0900

    add delete post button UI

--- src/components/series/seriesPostList.tsx
+++ src/components/series/seriesPostList.tsx
@@ -20,9 +20,10 @@ import {
   verticalListSortingStrategy,
 } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
-import { GripVertical, Plus } from 'lucide-react';
+import clsx from 'clsx';
+import { GripVertical, Plus, X } from 'lucide-react';
 import Link from 'next/link';
-import { ReactNode, useCallback, useState } from 'react';
+import { useCallback, useState } from 'react';
 import toast from 'react-hot-toast';
 
 export default function SeriesPostList({
@@ -92,16 +93,19 @@ export default function SeriesPostList({
       <SortableContext items={posts} strategy={verticalListSortingStrategy}>
         <div className='flex flex-col gap-8'>
           {posts.map((post, index) => (
-            <div key={post.id}>
-              <SortableContainer
-                id={post.id}
-                disabled={userId !== series.userId}
-              >
-                <SeriesPost post={post} index={index} />
-              </SortableContainer>
+            <div
+              key={post.id}
+              className='relative p-4 -m-4 rounded-xl group hover:bg-gray-100/50 transition-colors'
+            >
+              <SeriesPost
+                post={post}
+                index={index}
+                isOwner={userId === series.userId}
+              />
             </div>
           ))}
         </div>
+
         <button
           onClick={() => setIsAddDialogOpen(true)}
           className='mt-6 flex items-center justify-center gap-2 px-6 py-3 rounded-xl bg-gray-100 hover:bg-gray-200 text-gray-600 hover:text-gray-700 transition-colors font-medium mx-auto'
@@ -123,17 +127,21 @@ export default function SeriesPostList({
   );
 }
 
-function SortableContainer({
-  id,
-  disabled,
-  children,
+function SeriesPost({
+  post,
+  index,
+  isOwner,
 }: {
-  id: string;
-  disabled: boolean;
-  children: ReactNode;
+  post: SeriesProps['posts'][number];
+  index: number;
+  isOwner: boolean;
 }) {
+  const {
+    stat: { viewCount },
+  } = usePostStat({ postId: post.id });
+
   const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id, disabled });
+    useSortable({ id: post.id, disabled: !isOwner });
 
   const style = {
     transform: CSS.Transform.toString(transform),
@@ -141,54 +149,49 @@ function SortableContainer({
   };
 
   return (
-    <div
-      ref={setNodeRef}
-      style={style}
-      className={
-        'flex gap-4 items-center relative p-4 -m-4 rounded-xl hover:bg-gray-100/50 transition-colors'
-      }
-    >
-      <div className='flex-1 min-w-0'>{children}</div>
-      {!disabled && (
+    <div ref={setNodeRef} style={style} className='flex gap-4 items-center'>
+      <div className='flex-1 min-w-0'>
+        <Link href={`/read/${post.id}`} className='flex items-start gap-4'>
+          <div className='text-xl font-semibold text-gray-300 group-hover:text-gray-400 min-w-8 mt-0.5'>
+            {index + 1}
+          </div>
+          <div className='flex-1 flex flex-col gap-2'>
+            <div className='text-xl font-semibold text-gray-900 line-clamp-2'>
+              {post.title}
+            </div>
+            <div className='flex items-center gap-2 text-xs text-gray-500'>
+              <div>{post.createdAt}</div>
+              <Divider />
+              <div>조회 {viewCount}</div>
+            </div>
+          </div>
+        </Link>
+      </div>
+
+      {isOwner && (
+        <button
+          onClick={e => {
+            // onRemove?.(id);
+          }}
+          className='text-gray-300 group-hover:text-gray-400 hover:text-red-500 p-2 -m-2'
+        >
+          <X className='w-5 h-5' />
+        </button>
+      )}
+
+      {isOwner && (
         <div
           {...attributes}
           {...listeners}
-          className='cursor-grab active:cursor-grabbing text-gray-300 hover:text-gray-400 shrink-0 mt-0.5'
+          className={clsx(
+            'cursor-grab active:cursor-grabbing text-gray-300 group-hover:text-gray-400 shrink-0',
+            'p-2 -m-2 rounded-full hover:bg-gray-200'
+          )}
         >
           <GripVertical className='w-5 h-5' />
         </div>
       )}
     </div>
-  );
-}
-
-function SeriesPost({
-  post,
-  index,
-}: {
-  post: SeriesProps['posts'][number];
-  index: number;
-}) {
-  const {
-    stat: { viewCount },
-  } = usePostStat({ postId: post.id });
-
-  return (
-    <Link href={`/read/${post.id}`} className='flex items-start gap-4 group'>
-      <div className='text-xl font-semibold text-gray-300 group-hover:text-gray-400 min-w-8 mt-0.5'>
-        {index + 1}
-      </div>
-      <div className='flex-1 flex flex-col gap-2'>
-        <div className='text-xl font-semibold text-gray-900 line-clamp-2'>
-          {post.title}
-        </div>
-        <div className='flex items-center gap-2 text-xs text-gray-500'>
-          <div>{post.createdAt}</div>
-          <Divider />
-          <div>조회 {viewCount}</div>
-        </div>
-      </div>
-    </Link>
   );
 }
 
