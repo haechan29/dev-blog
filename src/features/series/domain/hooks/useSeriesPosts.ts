df7b4bcbbf89b9995681ccefa9d78ee49ff54c26
haechan29 <haechan.im@gmail.com>
2025-12-05 12:55:44 +0900

    add add//remoePostMutation

--- src/features/series/domain/hooks/useSeriesPosts.ts
+++ src/features/series/domain/hooks/useSeriesPosts.ts
@@ -1,10 +1,15 @@
 'use client';
 
+import * as PostClientService from '@/features/post/domain/service/postClientService';
 import * as SeriesClientService from '@/features/series/domain/service/seriesClientService';
-import { createProps } from '@/features/series/ui/seriesProps';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { createProps, SeriesProps } from '@/features/series/ui/seriesProps';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-export default function useSeriesPosts(userId: string, seriesId: string) {
+export default function useSeriesPosts(
+  userId: string,
+  seriesId: string,
+  initialData: SeriesProps
+) {
   const queryClient = useQueryClient();
 
   const { data: series } = useQuery({
@@ -13,9 +18,49 @@ export default function useSeriesPosts(userId: string, seriesId: string) {
       const series = await SeriesClientService.fetchSeries(userId, seriesId);
       return createProps(series);
     },
+    initialData,
+  });
+
+  const addPostMutation = useMutation({
+    mutationFn: (postId: string) => {
+      return PostClientService.updatePost({
+        postId,
+        seriesId,
+        seriesOrder: series.postCount,
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['series', seriesId, 'posts'],
+      });
+    },
+  });
+
+  const removePostMutation = useMutation({
+    mutationFn: (postId: string) => {
+      const updates = [
+        { id: postId, seriesId: null, seriesOrder: null },
+        ...series.posts
+          .filter(post => post.id !== postId)
+          .map((post, index) => ({
+            id: post.id,
+            seriesId,
+            seriesOrder: index,
+          })),
+      ];
+
+      return PostClientService.updatePostsInSeries(updates);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['series', seriesId, 'posts'],
+      });
+    },
   });
 
   return {
     posts: series?.posts,
+    addPostMutation,
+    removePostMutation,
   } as const;
 }
