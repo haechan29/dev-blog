84d861e9ee04b3fa6c510c4ce8e6149c488e1eae
haechan29 <haechan.im@gmail.com>
2025-12-16 12:08:50 +0900

    add toggleLike to useLike hook

--- src/features/post-interaction/data/repository/interactionRepository.ts
+++ src/features/post-interaction/data/repository/interactionRepository.ts
@@ -4,3 +4,11 @@ export async function getIsLiked(postId: string): Promise<boolean> {
   const response = await api.get(`/api/posts/${postId}/likes`);
   return response.data;
 }
+
+export async function addLike(postId: string): Promise<void> {
+  await api.post(`/api/posts/${postId}/likes`);
+}
+
+export async function removeLike(postId: string): Promise<void> {
+  await api.delete(`/api/posts/${postId}/likes`);
+}
--- src/features/post-interaction/hooks/useLike.ts
+++ src/features/post-interaction/hooks/useLike.ts
@@ -1,13 +1,33 @@
 'use client';
 
 import * as InteractionRepository from '@/features/post-interaction/data/repository/interactionRepository';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export default function useLike({ postId }: { postId: string }) {
+  const queryClient = useQueryClient();
+
   const { data: isLiked = false } = useQuery({
     queryKey: ['posts', postId, 'like'],
     queryFn: () => InteractionRepository.getIsLiked(postId),
   });
 
-  return { isLiked } as const;
+  const toggleLike = useMutation({
+    mutationFn: () =>
+      isLiked
+        ? InteractionRepository.removeLike(postId)
+        : InteractionRepository.addLike(postId),
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey: ['posts', postId, 'like'] });
+      const previous = queryClient.getQueryData(['posts', postId, 'like']);
+
+      queryClient.setQueryData(['posts', postId, 'like'], !isLiked);
+
+      return { previous };
+    },
+    onError: (_error, _variables, context) => {
+      queryClient.setQueryData(['posts', postId, 'like'], context?.previous);
+    },
+  });
+
+  return { isLiked, toggleLike } as const;
 }
