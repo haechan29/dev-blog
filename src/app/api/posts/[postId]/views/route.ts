e7fe75cba93776a1c2d0b15f068b065f85e99cff
haechan29 <haechan.im@gmail.com>
2025-12-16 09:52:56 +0900

    add view API

---
+++ src/app/api/posts/[postId]/views/route.ts
@@ -0,0 +1,36 @@
+import { insertView } from '@/features/post-interaction/data/queries/interactionQueries';
+import { UnauthorizedError } from '@/features/user/data/errors/userErrors';
+import { ApiError } from '@/lib/api';
+import { getUserId } from '@/lib/user';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: Promise<{ postId: string }> }
+) {
+  try {
+    const { postId } = await params;
+    const userId = await getUserId();
+
+    if (!userId) {
+      throw new UnauthorizedError('인증되지 않은 요청입니다');
+    }
+
+    const { readDuration } = await request.json();
+
+    await insertView(userId, postId, readDuration);
+
+    return NextResponse.json({ data: null });
+  } catch (error) {
+    console.error('조회 기록 요청이 실패했습니다', error);
+
+    if (error instanceof ApiError) {
+      return error.toResponse();
+    }
+
+    return NextResponse.json(
+      { error: '조회 기록 요청이 실패했습니다' },
+      { status: 500 }
+    );
+  }
+}
