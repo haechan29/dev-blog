7175074c7b06671ae7cd4c22ae7b40ac9472caa9
haechan29 <haechan.im@gmail.com>
2025-12-27 18:48:44 +0900

    hide image loading indicator on reader mode

--- src/components/md/imageWithCaption.tsx
+++ src/components/md/imageWithCaption.tsx
@@ -58,29 +58,70 @@ export default function ImageWithCaption({
     return () => clearInterval(interval);
   }, [status]);
 
-  return !src || isError ? (
-    <div
-      className='flex items-center justify-center p-4 rounded-xl bg-gray-200 text-gray-700 m-4'
-      aria-label='이미지를 불러올 수 없습니다'
-    >
-      {'이미지를 불러올 수 없습니다'}
-    </div>
-  ) : mode === 'viewer' ? (
-    <Image
-      data-image-with-caption
-      data-caption={caption}
-      data-start-offset={startOffset}
-      data-end-offset={endOffset}
-      src={src}
-      alt={alt}
-      width={1000}
-      height={1000}
-      objectFit='contain'
-      onError={() => setIsError(true)}
-      onLoad={() => setIsError(false)}
-      className='h-full'
-    />
-  ) : (
+  if (!src || isError) {
+    return (
+      <div
+        className='flex items-center justify-center p-4 rounded-xl bg-gray-200 text-gray-700 m-4'
+        aria-label='이미지를 불러올 수 없습니다'
+      >
+        {'이미지를 불러올 수 없습니다'}
+      </div>
+    );
+  }
+
+  if (mode === 'viewer') {
+    return (
+      <Image
+        data-image-with-caption
+        data-caption={caption}
+        data-start-offset={startOffset}
+        data-end-offset={endOffset}
+        src={src}
+        alt={alt}
+        width={1000}
+        height={1000}
+        objectFit='contain'
+        onError={() => setIsError(true)}
+        onLoad={() => setIsError(false)}
+        className='h-full'
+      />
+    );
+  }
+
+  if (mode === 'reader') {
+    return (
+      <div
+        data-image-with-caption
+        data-start-offset={startOffset}
+        data-end-offset={endOffset}
+        data-size={size}
+        className='flex flex-col gap-4'
+      >
+        <Image
+          src={src}
+          alt={alt}
+          width={1000}
+          height={1000}
+          onError={() => setIsError(true)}
+          onLoad={() => setIsError(false)}
+          className={clsx(
+            'h-auto',
+            size === 'large' ? 'w-full' : 'min-w-56 w-1/2'
+          )}
+        />
+
+        <div className='whitespace-pre-wrap'>
+          {caption
+            .split(/(?<!\\)#/)
+            .map(s => s.replace(/\\#/g, '#'))
+            .filter(Boolean)
+            .join('')}
+        </div>
+      </div>
+    );
+  }
+
+  return (
     <div
       data-image-with-caption
       data-start-offset={startOffset}
