4c88e6d0fb5d3fb91830528427058a17fa721055
haechan29 <haechan.im@gmail.com>
2026-02-12 08:14:32 +0900

    chore: add image unoptimized option to avoid vercel limit

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
