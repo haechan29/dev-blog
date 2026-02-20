add6fda7fa5aa0880fc9bbe291ee8b3ba93a579a
haechan29 <haechan.im@gmail.com>
2026-02-20 11:36:19 +0900

    refactor: revert dialogue component style

--- src/components/md/dialogue.tsx
+++ src/components/md/dialogue.tsx
@@ -26,10 +26,7 @@ export default function Dialogue({
   const [isError, setIsError] = useState(false);
   const initial = speaker.charAt(0).toUpperCase();
   const showImage = avatar && isValidImageSrc(avatar) && !isError;
-
   const color = colors[getColorIndex(speaker)];
-  const profileSize = 'w-8 h-8 lg:w-10 lg:h-10 xl:w-12 xl:h-12';
-  const textSize = 'text-sm lg:text-base xl:text-lg font-medium';
 
   useEffect(() => {
     setIsError(false);
@@ -39,7 +36,7 @@ export default function Dialogue({
     <div
       data-start-offset={startOffset}
       data-end-offset={endOffset}
-      className='my-4 lg:my-5 xl:my-6 flex gap-3 not-prose'
+      className='not-prose my-4 flex gap-3'
     >
       {showImage ? (
         <Image
@@ -49,18 +46,15 @@ export default function Dialogue({
           height={32}
           onError={() => setIsError(true)}
           className={clsx(
-            profileSize,
             ringColors[getColorIndex(speaker)],
-            'rounded-full object-cover shrink-0 ring-2'
+            'w-8 h-8 rounded-full object-cover shrink-0 ring-2'
           )}
         />
       ) : (
         <div
           className={clsx(
-            profileSize,
-            textSize,
             color,
-            'rounded-full shrink-0 flex items-center justify-center text-white'
+            'w-8 h-8 rounded-full shrink-0 flex items-center justify-center text-white text-sm font-medium'
           )}
         >
           {initial}
@@ -68,7 +62,12 @@ export default function Dialogue({
       )}
 
       <div className='flex-1'>
-        <div className={clsx(textSize, textColors[getColorIndex(speaker)])}>
+        <div
+          className={clsx(
+            'text-xs font-medium',
+            textColors[getColorIndex(speaker)]
+          )}
+        >
           {speaker}
         </div>
         <div className='text-gray-900'>{children}</div>
